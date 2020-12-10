b4b8860a7ab3b6dc39665a42a97e44a0347bf2ae
Nico Deblauwe <nico@bluepundit.eu>
2020-12-10 16:09:46 +0100

    the simple e-mail

---
+++ resources/views/mail/test.blade.php
@@ -0,0 +1,8 @@
+Hello,<br/>
+
+This one behaves as expected<br/><br/>
+
+@{{{pm:unsubscribe}}}<br/>
+
+<a href="@{{{pm:unsubscribe}}}"> test </a>
+
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,14 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('mail', function() {
+    $dummy = App\Models\User::firstOrCreate([
+        'email'=> 'dummy@dummy.du',
+        ],[
+        'name' => 'dummy',
+        'password' => \Illuminate\Support\Facades\Hash::make('password'),
+    ]);
+
+    \Illuminate\Support\Facades\Mail::to($dummy)->send(new \App\Mail\TestMail());
+});
